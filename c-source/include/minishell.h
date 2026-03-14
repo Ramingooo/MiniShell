ed578ca5a5333baab0a5a16b7df927a73b9a1e56
Ramingooo <Ramingooo@github.com>
2026-03-14 02:09:38 +0000

    C code changes

--- c-source/include/minishell.h
+++ c-source/include/minishell.h
@@ -1,19 +1,11 @@
 #ifndef MINISHELL_H
 # define MINISHELL_H
 
-# include <stdio.h>
-# include <stdlib.h>
 # include <unistd.h>
-# include <string.h>
-# include <sys/wait.h>
-# include <errno.h>
+# include <stdlib.h>
 
-# define PROMPT "\033[1;32mminishell\033[0m$ "
-# define BUFFER_SIZE 1024
-
-void display_prompt(void);
-void execute_command(char **args);
-char **parse_command(char *line);
-void free_args(char **args);
+void	ft_putchar(char c);
+void	ft_putstr(char *s);
+size_t	ft_strlen(char *s);
 
 #endif
--- c-source/src/main.c
+++ c-source/src/main.c
@@ -1,56 +1,26 @@
 #include "../include/minishell.h"
 
-void display_prompt(void) {
-    printf("%s", PROMPT);
-    fflush(stdout);
+size_t	ft_strlen(char *s)
+{
+	size_t	i = 0;
+	while (s && s[i])
+		i++;
+	return (i);
 }
 
-char **parse_command(char *line) {
-    char **args = malloc(64 * sizeof(char*));
-    char *token;
-    int i = 0;
-
-    token = strtok(line, " \t\n\r");
-    while (token != NULL) {
-        args[i++] = strdup(token);
-        token = strtok(NULL, " \t\n\r");
-    }
-    args[i] = NULL;
-    return args;
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
 }
 
-void free_args(char **args) {
-    for (int i = 0; args[i]; i++)
-        free(args[i]);
-    free(args);
+void	ft_putstr(char *s)
+{
+	if (s)
+		write(1, s, ft_strlen(s));
 }
 
-void execute_command(char **args) {
-    if (args[0] == NULL) return;
-    if (strcmp(args[0], "exit") == 0) {
-        free_args(args);
-        exit(0);
-    }
-    pid_t pid = fork();
-    if (pid == 0) {
-        if (execvp(args[0], args) == -1)
-            perror("minishell");
-        exit(EXIT_FAILURE);
-    } else if (pid < 0)
-        perror("minishell");
-    else
-        wait(NULL);
-}
-
-int main(void) {
-    char line[BUFFER_SIZE];
-    char **args;
-    while (1) {
-        display_prompt();
-        if (fgets(line, BUFFER_SIZE, stdin) == NULL) break;
-        args = parse_command(line);
-        execute_command(args);
-        free_args(args);
-    }
-    return 0;
+int	main(void)
+{
+	ft_putstr("Hello World\n");
+	return (0);
 }
